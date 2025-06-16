5c75934a9f9f9c8f1a9834b01860e5be0a7bbf1e
yuanlei <2365933499@qq.com>
2025-06-17 07:54:42 +0800

    githun部署测试

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 import { defineConfig, loadEnv } from 'vite'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
-
+  console.log('VITE_PLATFORM:', env.VITE_PLATFORM);
   // 默认为根路径
   let base = '/'
 
