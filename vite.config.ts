bfb8b6f196a59b72d7c82b110b4502e03acd172b
yuanlei <2365933499@qq.com>
2025-06-17 08:20:15 +0800

    github部署文件修改

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import { defineConfig, loadEnv } from 'vite'
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd(),'')
   console.log('VITE_PLATFORM:', env.VITE_PLATFORM);
   // 默认为根路径
   let base = '/'
