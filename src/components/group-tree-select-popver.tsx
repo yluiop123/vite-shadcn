cf04095ca57a0e6c2a8629fc3915f066bb579105
yuanlei <2365933499@qq.com>
2025-08-30 22:38:19 +0800

    处理组织加载问题

--- src/components/group-tree-select-popver.tsx
+++ src/components/group-tree-select-popver.tsx
@@ -4,11 +4,11 @@ import { useUserStore } from '@/store';
 import { useEffect, useState } from "react";
 import { TreeNode, TreeSelectPopover } from './tree-select';
 export function GroupTreeSelectPopover( props: {defaultValue?:string,onChange:(node:TreeNode[])=>void}) {
-  const [arrayData, setArrayData] = useState<TreeNode[]>([{id:'',name:'...',parentId:'',level:0,order:0}]);
+  const [arrayData, setArrayData] = useState<TreeNode[]>([]);
   const {userInfo}=useUserStore();
   const {onChange,defaultValue}=props;
   const groupId=userInfo?.group;
-  const [choose, setChoose] = useState<TreeNode>(arrayData[0]);
+  const [choose, setChoose] = useState<TreeNode>({ } as TreeNode);
   useEffect(()=>{
     axios.get('/common/groups/'+groupId).then(res=>{
       setArrayData(res.data.data);
@@ -16,8 +16,8 @@ export function GroupTreeSelectPopover( props: {defaultValue?:string,onChange:(n
       const defaultChoose = arrayData.find((item) => item.id === defaultValue)||res.data.data[0];
       setChoose(defaultChoose);
     })
-  },[groupId,defaultValue])
+  },[groupId,defaultValue,])
  
   return (
-    <TreeSelectPopover onChange={onChange} data={arrayData} choose={choose} setChoose={setChoose} />)
+    arrayData.length>0&&<TreeSelectPopover onChange={onChange} data={arrayData} choose={choose} setChoose={setChoose} />)
 }
