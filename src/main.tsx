3d55e9de031b689d2d42dfeaa4926c678851feb5
yuanlei <2365933499@qq.com>
2025-09-01 12:11:20 +0800

    初始化msw

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import App from '@/App';
+import initMSW from '@/mock';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-
+const mockEnable = (import.meta.env.VITE_MOCK_ENABLE||'true')=='true';
+if(mockEnable){
+  initMSW();
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
