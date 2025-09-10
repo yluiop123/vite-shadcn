fca700f9b8534c7bef6289b9c6b28105fcf3ac08
yuanlei <2365933499@qq.com>
2025-09-10 21:41:38 +0800

    msw405问题测试

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,17 +1,16 @@
-import { setupWorker } from 'msw/browser';
-import groupHandlers from './components/group';
-import loginUserHandlers from './login/user';
-import systemGroupHandlers from './system/group';
-import systemRoleHandlers from './system/role';
-import systemUserHandlers from './system/user';
+import { setupWorker } from "msw/browser";
+import groupHandlers from "./components/group";
+import loginUserHandlers from "./login/user";
+import systemGroupHandlers from "./system/group";
+import systemRoleHandlers from "./system/role";
+import systemUserHandlers from "./system/user";
 const mockHandlers = [
-    ...loginUserHandlers,
-    ...groupHandlers,
-    ...systemUserHandlers,
-    ...systemRoleHandlers,
-    ...systemGroupHandlers
-]
-
+  ...loginUserHandlers,
+  ...groupHandlers,
+  ...systemUserHandlers,
+  ...systemRoleHandlers,
+  ...systemGroupHandlers,
+];
 
 export default async function initMSW() {
   const worker = setupWorker(...mockHandlers);
@@ -23,8 +22,11 @@ export default async function initMSW() {
 
   // 启动 MSW
   await worker.start({
-    serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js?ts=${Date.now()}` },
-    onUnhandledRequest: 'warn', // 未匹配请求会在 console 警告
+    serviceWorker: {
+      url: `${import.meta.env.BASE_URL}mockServiceWorker.js?ts=${Date.now()}`,
+      options: { updateViaCache: "none" },
+    },
+    onUnhandledRequest: "warn", // 未匹配请求会在 console 警告
   });
   return worker;
 }
