689163b68090d666358b28706418bc1bb790a8d0
yuanlei <2365933499@qq.com>
2025-09-10 08:25:41 +0800

    mock生产失效问题处理

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -20,14 +20,14 @@ export default async function initMSW() {
   }
 
   // 注销旧 SW
-  const registrations = await navigator.serviceWorker.getRegistrations();
-  for (const reg of registrations) {
-    await reg.unregister();
-  }
+  // const registrations = await navigator.serviceWorker.getRegistrations();
+  // for (const reg of registrations) {
+  //   await reg.unregister();
+  // }
 
   // 启动 MSW
   await worker.start({
-    serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js` },
+    serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js?ts=${Date.now()}` },
     onUnhandledRequest: 'bypass', // 未匹配请求会在 console 警告
   });
   return worker;
