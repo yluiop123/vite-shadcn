89c5f76682c37afaeb1c3e006727fa1a45267e7a
yuanlei <2365933499@qq.com>
2025-09-27 22:16:35 +0800

    分割线自定义

--- src/pages/component/general/index.tsx
+++ src/pages/component/general/index.tsx
@@ -2,18 +2,23 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { lazy } from "react";
 const ButtonPage = lazy(() => import('./button'));
 const FloatButtonPage = lazy(() => import('./float-button'));
+const DividerPage  = lazy(() => import('./divide'));
 export default function General() {
   return (
         <Tabs defaultValue="button" className="p-3">
             <TabsList>
                 <TabsTrigger value="button">Button</TabsTrigger>
                 <TabsTrigger value="floatButton">FloatButton</TabsTrigger>
+                <TabsTrigger value="divider">Divider</TabsTrigger>
             </TabsList>
             <TabsContent value="button">
                 <ButtonPage />
             </TabsContent>
              <TabsContent value="floatButton">
                 <FloatButtonPage />
+            </TabsContent>
+             <TabsContent value="divider">
+                <DividerPage />
             </TabsContent>
         </Tabs>
   );
