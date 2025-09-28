932199f7dc3b6297681e9ea0bd79697a15b2d2d9
yuanlei <2365933499@qq.com>
2025-09-28 21:58:04 +0800

    增加  Resizable组件示例

--- src/pages/component/general/index.tsx
+++ src/pages/component/general/index.tsx
@@ -3,6 +3,7 @@ import { lazy } from "react";
 const ButtonPage = lazy(() => import('./button'));
 const FloatButtonPage = lazy(() => import('./float-button'));
 const DividerPage  = lazy(() => import('./divide'));
+const ResizablePage = lazy(() => import('./resizable'));
 export default function General() {
   return (
         <Tabs defaultValue="button" className="p-3">
@@ -10,6 +11,7 @@ export default function General() {
                 <TabsTrigger value="button">Button</TabsTrigger>
                 <TabsTrigger value="floatButton">FloatButton</TabsTrigger>
                 <TabsTrigger value="divider">Divider</TabsTrigger>
+                <TabsTrigger value="resizable">Resizable</TabsTrigger>
             </TabsList>
             <TabsContent value="button">
                 <ButtonPage />
@@ -19,6 +21,9 @@ export default function General() {
             </TabsContent>
              <TabsContent value="divider">
                 <DividerPage />
+            </TabsContent>
+             <TabsContent value="resizable">
+                <ResizablePage />
             </TabsContent>
         </Tabs>
   );
