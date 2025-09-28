1d6dac80ec7cc473ca4c04cb46c390e5c289c119
yuanlei <2365933499@qq.com>
2025-09-28 23:11:37 +0800

    Typography组件

--- src/pages/component/general/index.tsx
+++ src/pages/component/general/index.tsx
@@ -4,20 +4,25 @@ const ButtonPage = lazy(() => import('./button'));
 const FloatButtonPage = lazy(() => import('./float-button'));
 const DividerPage  = lazy(() => import('./divide'));
 const ResizablePage = lazy(() => import('./resizable'));
+const TypographyPage = lazy(() => import('./typography'));
 export default function General() {
   return (
         <Tabs defaultValue="button" className="p-3">
             <TabsList>
                 <TabsTrigger value="button">Button</TabsTrigger>
                 <TabsTrigger value="floatButton">FloatButton</TabsTrigger>
+                <TabsTrigger value="typography">Typography</TabsTrigger>
                 <TabsTrigger value="divider">Divider</TabsTrigger>
-                <TabsTrigger value="resizable">Resizable</TabsTrigger>
+                <TabsTrigger value="resizable">Resizable</TabsTrigger>            
             </TabsList>
             <TabsContent value="button">
                 <ButtonPage />
             </TabsContent>
              <TabsContent value="floatButton">
                 <FloatButtonPage />
+            </TabsContent>
+            <TabsContent value="typography">
+                <TypographyPage />
             </TabsContent>
              <TabsContent value="divider">
                 <DividerPage />
