377afcf2008a29f67f4d249cd5aa172285ef83c5
yuanlei <2365933499@qq.com>
2026-01-31 15:10:50 +0800

    优化菜单样式

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -11,16 +11,31 @@ import {
   SidebarHeader,
   SidebarRail,
 } from "@/components/ui/sidebar"
+import { cn } from "@/lib/utils"
 import { routeSetting } from "@/routes"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
+        Vite-Shadcn
       </SidebarHeader>
-      <SidebarContent>
+      
+      <SidebarContent 
+        className={cn(
+          "flex-1 overflow-y-auto",
+          // 1. 基础样式：定义滚动条宽度和轨道颜色
+          "scrollbar-thin scrollbar-track-transparent",
+          // 2. 滑块样式：圆角及颜色 (适配暗黑模式)
+          "scrollbar-thumb-muted-foreground/20 hover:scrollbar-thumb-muted-foreground/40",
+          "dark:scrollbar-thumb-slate-700 dark:hover:scrollbar-thumb-slate-600",
+          // 3. 细节：滑块圆角
+          "scrollbar-thumb-rounded-full"
+        )}
+      >
         <NavMain items={routeSetting} />
       </SidebarContent>
+
       <SidebarFooter>
         <NavUser/>
       </SidebarFooter>
--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -11,7 +11,7 @@ import { NavItem } from "@/routes";
 export function NavMain({items}: {items: NavItem[]}) {
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Vite-Shadcn</SidebarGroupLabel>
+      <SidebarGroupLabel>Menu</SidebarGroupLabel>
       <SidebarMenu>
         {items.map((item, index) => (          
            <SidebarMenuTree item={item} key={index}></SidebarMenuTree>
