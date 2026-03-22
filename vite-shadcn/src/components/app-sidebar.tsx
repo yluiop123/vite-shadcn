7abfd38a70531e167c8e5bb0a6e5abd551d156cc
yuanlei <2365933499@qq.com>
2026-03-22 18:44:55 +0800

    vite-shadcn 侧边栏折叠图片隐藏

--- vite-shadcn/src/components/app-sidebar.tsx
+++ vite-shadcn/src/components/app-sidebar.tsx
@@ -22,7 +22,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-          <Avatar className="h-15 w-15 rounded-lg ml-15">
+          <Avatar className="h-15 w-15 rounded-lg ml-15 group-data-[state=collapsed]:hidden">
             <AvatarImage src="orange.png" />
             <AvatarFallback className="rounded-lg">Orange</AvatarFallback>
           </Avatar>
